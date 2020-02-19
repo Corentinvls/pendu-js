a493cd2f2265f9df0c646f9c6e3791fec463efc6
vallois <corentin.vallois@edu.itescia.fr>
2020-02-19 14:47:38 +0100

    intialising project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './redux/reducers'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Accueil from './components/Accueil';
+import Configuration from './components/Configuration';
+import APropos from './components/APropos';
+
+
+
+const store = createStore(reducer);
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+    };
+    console.log('initial state', store.getState());
+
+  }
+
+  render() {
+    return (
+        <Provider store={store}>
+
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Accueil</Link>
+                  </li>
+                  <li>
+                    <Link to="/configuration">Configuration</Link>
+                  </li>
+                  <li>
+                    <Link to="/aPropos">A propos</Link>
+                  </li>
+                </ul>
+              </nav>
+              <Switch>
+                <Route exact path="/">
+                  <Accueil/>
+                </Route>
+                <Route path="/configuration">
+                  <Configuration/>
+                </Route>
+                <Route path="/aPropos">
+                  <APropos/>
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </Provider>
+
+    );
+  }
 }
-
-export default App;
---
+++ src/components/APropos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class APropos extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>Corentin Vallois</h2>
+                <h2>Guillaume Pinheiro</h2>
+                <h2>Adrian Duveau</h2>
+                <h2>Marine Bijon</h2>
+            </div>
+        );
+    }
+}
