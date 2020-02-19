c727a9d4ca9956e0d1665edd16cdeb98856bfc5f
vallois <corentin.vallois@edu.itescia.fr>
2020-02-19 16:57:40 +0100

    f*** pendu Speed click is better

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,79 +1,107 @@
-
 import React from 'react';
 import '../App.css';
+
 export default class Accueil extends React.Component {
+
 
     constructor() {
         super();
         this.state = {
-            alphabet : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-                "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"],
-            wordAnswer: "",
-            wordAsk:'',
-            wrong: 0,
-
-        };
+            time: 0,
+            isOn: false,
+            start: 0
+        }
+        this.startTimer = this.startTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+        this.resetTimer = this.resetTimer.bind(this)
     }
 
-    /*generateWord() {
-        const words = ["Acheteur", "Acrobate", "Acteur", "Actrice", "Actuaire", "Affreteur", "Agriculteur", "Alchimiste", "Alpiniste",
-            "Ambassadeur", "Ambulancier", "Analyste", "Anesthesiste", "Animateur"];
+    startTimer() {
+        this.setState({
+            isOn: true,
+            time: this.state.time,
+            start: Date.now() - this.state.time
+        });
+        this.timer = setInterval(() => this.setState({
+            time: Math.round((Date.now() - this.state.start)/100)/10
+        }), 50);
+    }
 
-        let wordIndex = Math.floor(Math.random() * words.length);
-        let word = words[wordIndex];
-        let worlab = document.createElement('h2');
-        worlab.setAttribute("id", "mot");
-        document.getElementById("mystery").appendChild(worlab);
-        let wordAnswer = word.split("");
-        console.log(wordAnswer);
-        let wordAsk = [];
+    stopTimer() {
+        this.setState({isOn: false});
+        clearInterval(this.timer)
+    }
 
-        for (let i = 0; i < wordAnswer.length; i++) {
-            if (i === 0 || i === (wordAnswer.length - 1)) {
-                wordAsk.push(wordAnswer[i]);
-            } else {
-                wordAsk.push(" _ ");
-            }
+    resetTimer() {
+        this.setState({time: 0, isOn: false})
+    }
+
+    render() {
+        {
+
+            let start = (this.state.time == 0) ?
+                <button onClick={this.startTimer}>start</button> :
+                null
+            let stop = (this.state.time == 0 || !this.state.isOn) ?
+                null :
+                <button onClick={this.stopTimer}>stop</button>
+            let resume = (this.state.time == 0 || this.state.isOn) ?
+                null :
+                <button onClick={this.startTimer}>resume</button>
+            let reset = (this.state.time == 0 || this.state.isOn) ?
+                null :
+                <button onClick={this.resetTimer}>reset</button>
         }
 
-        wordAsk.forEach(function (el, index) {
-
-            let span = document.createElement('span');
-            span.innerHTML = el.toString();
-            span.setAttribute("id", index.toString());
-            document.getElementById("mot").appendChild(span);
-
-        });
-
-    }*/
-
-
-    render () {
         return (
-        <div>
-        <header >
-            <h1 id="maintitle">Le Pendu</h1>
-            <button id="newgame" type="button" >Nouvelle partie</button>
-            <hr/>
-        </header>
-        <div id="game">
-            <div id="image"> </div>
-            <div id="mystery"> </div>
-            <div id="letters">
-                {this.state.alphabet.map((letter, i) =>
-                    (
-                        <button key={i}/* onClick={this.selectedLetter(letter)}*/>{letter}</button>
-                    )
-                )}
+            <div>
+                <header>
+                    <h1 id="maintitle">Speed Click</h1>
+                    <button id="newgame" type="button"onClick={this.startTimer}>Nouvelle partie</button>
+                    <hr/>
+                </header>
+                <div>
+                    <h3>timer: {(this.state.time)}</h3>
+                    <button onClick={this.startTimer}>start</button>
+                    <button onClick={this.stopTimer}>stop</button>
+                    <button onClick={this.resetTimer}>reset</button>
+                </div>
             </div>
-            <div id="mistake"> </div>
-            <h2> </h2>
-        </div>
-
-        </div>
-        );
+        )
     }
+
+
 }
 
 
+/*
+const ms = require('pretty-ms')
+class Timer extends React.Component {
+    constructor(props){
+        super(props)
+        this.state
+    render() {
+        let start = (this.state.time == 0) ?
+            <button onClick={this.startTimer}>start</button> :
+            null
+        let stop = (this.state.time == 0 || !this.state.isOn) ?
+            null :
+            <button onClick={this.stopTimer}>stop</button>
+        let resume = (this.state.time == 0 || this.state.isOn) ?
+            null :
+            <button onClick={this.startTimer}>resume</button>
+        let reset = (this.state.time == 0 || this.state.isOn) ?
+            null :
+            <button onClick={this.resetTimer}>reset</button>
+        return(
+            <div>
+                <h3>timer: {ms(this.state.time)}</h3>
+                {start}
+                {resume}
+                {stop}
+                {reset}
+            </div>
+        )
+    }
+}*/
 
