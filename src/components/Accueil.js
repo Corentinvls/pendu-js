1efad07152b3abbe9df1dd3aff49460f78fd527c
vallois <corentin.vallois@edu.itescia.fr>
2020-02-19 19:38:28 +0100

    speed click done

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -7,65 +7,65 @@ export default class Accueil extends React.Component {
     constructor() {
         super();
         this.state = {
-            time: 0,
+            time: 30,
             isOn: false,
-            start: 0
-        }
+            start: 0,
+            score: 0
+        };
         this.startTimer = this.startTimer.bind(this);
         this.stopTimer = this.stopTimer.bind(this);
-        this.resetTimer = this.resetTimer.bind(this)
+        this.click = this.click.bind(this);
+
     }
 
     startTimer() {
         this.setState({
             isOn: true,
             time: this.state.time,
-            start: Date.now() - this.state.time
+            start: Date.now() - this.state.time,
+            score: 0
         });
+        this.timer = setInterval(() => this.setState(this.stopTimer()), 50);
         this.timer = setInterval(() => this.setState({
-            time: Math.round((Date.now() - this.state.start)/100)/10
+            time: (3 - Math.round((Date.now() - this.state.start) / 100) / 10).toFixed(1)
         }), 50);
     }
 
-    stopTimer() {
-        this.setState({isOn: false});
-        clearInterval(this.timer)
-    }
 
-    resetTimer() {
-        this.setState({time: 0, isOn: false})
+
+    stopTimer() {
+        if (this.state.time <= 0) {
+            this.setState({isOn: false});
+            clearInterval(this.timer);
+            this.setState({time: 30})
+        }
+    }
+    click() {
+        if(this.state.isOn){
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+
     }
 
     render() {
-        {
-
-            let start = (this.state.time == 0) ?
-                <button onClick={this.startTimer}>start</button> :
-                null
-            let stop = (this.state.time == 0 || !this.state.isOn) ?
-                null :
-                <button onClick={this.stopTimer}>stop</button>
-            let resume = (this.state.time == 0 || this.state.isOn) ?
-                null :
-                <button onClick={this.startTimer}>resume</button>
-            let reset = (this.state.time == 0 || this.state.isOn) ?
-                null :
-                <button onClick={this.resetTimer}>reset</button>
-        }
 
         return (
             <div>
                 <header>
                     <h1 id="maintitle">Speed Click</h1>
-                    <button id="newgame" type="button"onClick={this.startTimer}>Nouvelle partie</button>
+                    <h2>timer: {(this.state.time)}</h2>
+                    <h2>score: {(this.state.score)}</h2>
+                    <button id="newgame" type="button" onClick={this.startTimer}>Nouvelle partie</button>
                     <hr/>
+                    <div>
+                        <button id="newgame" type="button" onClick={this.click} disabled={!this.state.isOn}>Bourrine
+                            moi
+                        </button>
+                    </div>
                 </header>
-                <div>
-                    <h3>timer: {(this.state.time)}</h3>
-                    <button onClick={this.startTimer}>start</button>
-                    <button onClick={this.stopTimer}>stop</button>
-                    <button onClick={this.resetTimer}>reset</button>
-                </div>
+
             </div>
         )
     }
@@ -74,7 +74,9 @@ export default class Accueil extends React.Component {
 }
 
 
-/*
+/*<button onClick={this.startTimer}>start</button>
+                    <button onClick={this.stopTimer}>stop</button>
+                    <button onClick={this.resetTimer}>reset</button>
 const ms = require('pretty-ms')
 class Timer extends React.Component {
     constructor(props){
