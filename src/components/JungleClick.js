7d7a9f0c2396d74bc48c84a5685ae99d49f7fe0a
vallois <corentin.vallois@edu.itescia.fr>
2020-02-20 15:13:56 +0100

    merge fix

--- src/components/JungleClick.js
+++ src/components/JungleClick.js
@@ -11,7 +11,6 @@ export default class JungleClick extends React.Component {
             isOn: false,
             start: 0,
             score: 0,
-            color: "White"
         };
         this.startTimer = this.startTimer.bind(this);
         this.stopTimer = this.stopTimer.bind(this);
@@ -26,13 +25,14 @@ export default class JungleClick extends React.Component {
             time: this.state.time,
             start: Date.now() - this.state.time,
             score: 0,
-            color: this.randColor()
-        });
 
+
+        });
         this.timer = setInterval(() => this.setState({
             time: (Math.round((Date.now() - this.state.start) / 100) / 10).toFixed(1)
-        }), 50);
-        this.timerColor = setInterval(() => this.setState({color: this.randColor()}), Math.floor(Math.random() * 3000) + 1000);
+        }), Math.floor(Math.random() * 500) + 750);
+
+
     }
 
 
@@ -40,70 +40,61 @@ export default class JungleClick extends React.Component {
         this.setState({isOn: false, time: 0});
         clearInterval(this.timer);
         clearInterval(this.timerColor);
+        clearInterval(this.deadTimer);
     }
 
     randColor() {
-        let couleur = ["Vert", "Jaune", "Rouge", "Bleu"];
-        let b = Math.floor(Math.random() * 4);
+        var b = Math.floor(Math.random() * 8);
+        let couleur = ["Vert", "Jaune", "Rouge", "Bleu", "Violet", "Orange", "Marron", "Rose"];
         return couleur[b];
     }
 
-    click(value) {
-        console.log("coucou");
-        console.log(value);
-        if (value === this.state.color) {
+    click(value1, value2, value3, value4) {
+        if (value1 === value2 || value1 === value3 || value1 === value4) {
+
+            clearInterval(this.timer);
+            this.timer = setInterval(() => this.setState({
+                time: (Math.round((Date.now() - this.state.start) / 100) / 10).toFixed(1)
+            }), Math.floor(Math.random() * 500) + 750);
+
             this.setState({
                 score: this.state.score + 1
             });
+
         } else {
             this.stopTimer();
         }
+
     }
 
     render() {
-        let btnClass;
-        switch (this.state.color) {
-            case "Vert" :
-                btnClass = "Vert";
-                break;
-            case "Jaune" :
-                btnClass = "Jaune";
-                break;
-            case "Rouge" :
-                btnClass = "Rouge";
-                break;
-            case "Bleu" :
-                btnClass = "Bleu";
-                break;
-            default:
-                btnClass = "White";
-                break;
-
-        }
+        var button1 = this.randColor();
+        let button2 = this.randColor();
+        let button3 = this.randColor();
+        let button4 = this.randColor();
         return (
             <div>
                 <header>
                     <h1 id="maintitle">Jungle Click</h1>
-                    <h2>timer: {(this.state.time)}</h2>
                     <h2>score: {(this.state.score)}</h2>
                     <button id="newgame" type="button" onClick={this.startTimer}>Nouvelle partie</button>
                     <hr/>
-                    <span className={btnClass}>look at me</span>
                     <div>
-
-
-                        <button id="Vert" type="button" className={"Vert"} onClick={event => this.click("Vert")}
-                                value={"Vert"}
-                                disabled={!this.state.isOn}>Vert
+                        <button type="button" className={button1} value={button1}
+                                onClick={event => this.click(button1, button2, button3, button4)}
+                                disabled={!this.state.isOn}>BLABLA
                         </button>
-                        <button type="button" className={"Jaune"} onClick={event => this.click("Jaune")} value={"Jaune"}
-                                disabled={!this.state.isOn}>Jaune
+                        <button type="button" className={button2} value={button2}
+                                onClick={event => this.click(button2, button3, button4, button1)}
+                                disabled={!this.state.isOn}>BLABLA
                         </button>
-                        <button type="button" className={"Rouge"} onClick={event => this.click("Rouge")} value={"Rouge"}
-                                disabled={!this.state.isOn}>Rouge
+                        <button type="button" className={button3} value={button3}
+                                onClick={event => this.click(button3, button2, button4, button1)}
+                                disabled={!this.state.isOn}>BLABLA
                         </button>
-                        <button type="button" className={"Bleu"} onClick={event => this.click("Bleu")} value={"Bleu"}
-                                disabled={!this.state.isOn}>Bleu
+                        <button type="button" className={button4} value={button4}
+                                onClick={event => this.click(button4, button3, button2, button1)}
+                                disabled={!this.state.isOn}>BLABLA
                         </button>
                     </div>
                 </header>
